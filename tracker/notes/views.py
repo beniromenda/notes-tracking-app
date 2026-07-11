e6d767541ed01dbd06b0174bfa0e0e4bd137df97
benir-o <beniromenda@gmail.com>
2026-07-12 00:23:47 +0300

    Automatically generating forms

--- tracker/notes/views.py
+++ tracker/notes/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect,get_object_or_404
 from .models import Note
+from .forms import NoteForm
 
 # Create your views here.
 def note_list(request):
@@ -7,21 +8,30 @@ def note_list(request):
     return render(request, 'notes/list.html',{'notes': notes})
 
 def note_create(request):
-    if request.method=='POST':
-        Note.objects.create(title=request.POST['title'], content=request.POST['content']);
-        return redirect('note_list')
-    return render(request, 'notes/create.html')
+    if request.method == 'POST':
+        form = NoteForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('note_list')
+    else:
+        form = NoteForm()
+
+    return render(request, 'notes/create.html', {'form': form})
 
 def note_update(request, pk):
     note=get_object_or_404(Note, pk=pk)
-    if request.method=="POST":
-        note.title = request.POST['title']
-        note.content = request.POST['content']
-        note.save()
-        return redirect('note_list')
-    return render (request, 'notes/update.html',{'note':note})
 
-def note_delete(request, pk):
+    if request.method=="POST":
+        form=NoteForm(request.POST, instance=note)
+        if form.is_valid():
+            form.save()
+            return redirect('note_list')
+    else:
+        form=NoteForm(instance=note)
+    
+    return render (request, 'notes/update.html',{'form':form, 'note':note})
+
+def note_delete(request,pk):
     note=get_object_or_404(Note, pk=pk)
     note.delete()
     return redirect ('note_list')
