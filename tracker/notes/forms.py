a11fcb29adf620c866f4e6761731d0c8c4f27e2b
beniromenda <benir.omenda@strathmore.edu>
2026-07-14 00:14:11 +0300

    Login Functionality added. Assigned notes to a user

--- tracker/notes/forms.py
+++ tracker/notes/forms.py
@@ -1,6 +1,5 @@
 from django import forms
 from .models import Note
-
 class NoteForm(forms.ModelForm):
     class Meta:
         model=Note
--- tracker/notes/views.py
+++ tracker/notes/views.py
@@ -3,19 +3,27 @@ from .models import Note
 from .forms import NoteForm
 from django.views.generic import  CreateView, UpdateView, DeleteView, ListView
 from django.urls import reverse_lazy
-
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
-class NoteListView(ListView):
+class NoteListView(LoginRequiredMixin, ListView):
     model=Note
     template_name='notes/list.html'
     context_object_name='notes'
+    #Restricting views to logged in users
+    def get_queryset(self):
+        return Note.objects.filter(user=self.request.user)
 
-class NoteCreateView(CreateView):
+class NoteCreateView(LoginRequiredMixin, CreateView):
     model=Note
     form_class=NoteForm
     template_name='notes/create.html'
     success_url=reverse_lazy('note_list')
+
+    #When the user signs in, we need to auto assign this note to the specific user
+    def form_valid(self, form):
+        form.instance.user= self.request.user
+        return super().form_valid(form)
 
 class NoteUpdateView(UpdateView):
     model=Note
